eb4b9fae60dcbeb245afcb5f7362cc19b79ef3da
testnodeemail123 <testnodeemail123@gmail.com>
2017-12-01 06:46:20 +0530

    updated server routes and package.json

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 //  OpenShift sample Node application
+
+var mongoose = require('mongoose');
+var configDB = require('./config/database.js');
+mongoose.Promise = global.Promise;
+
+
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
@@ -8,53 +14,98 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+
+var cors = require('cors');
+app.use(cors());
+
+var bodyparser = require('body-parser');
+app.use(bodyparser.json()); 
+app.use(bodyparser.urlencoded({
+   parameterLimit: 10, 
+   limit: '50mb',      
+   extended: true      
+ }));
+
+app.set('superSecret', '#123!443'); //used for jwt token
+
+
+app.use(express.static(__dirname + '/public'));
+
+var timeout = require('connect-timeout')
+app.use(timeout('10s')) //status 503 goes in http response if timedout
+
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+//     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+//     mongoURLLabel = "";
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+// if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+//   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+//       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+//       mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+//       mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+//       mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+//       mongoUser = process.env[mongoServiceName + '_USER'];
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+//   if (mongoHost && mongoPort && mongoDatabase) {
+//     mongoURLLabel = mongoURL = 'mongodb://';
+//     if (mongoUser && mongoPassword) {
+//       mongoURL += mongoUser + ':' + mongoPassword + '@';
+//     }
+//     // Provide UI label that excludes user id and pw
+//     mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+//     mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
 
-  }
-}
+//   }
+// }
+mongoURL = configDB.url
+
+
 var db = null,
     dbDetails = new Object();
 
 var initDb = function(callback) {
   if (mongoURL == null) return;
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
+  // var mongodb = require('mongodb');
+  // if (mongodb == null) return;
 
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
+  mongoose.connect(mongoURL,{ useMongoClient: true },function(err){
+    if(err) throw err;
 
-    db = conn;
+    db = mongoose.connection;// db = conn;
     dbDetails.databaseName = db.databaseName;
     dbDetails.url = mongoURLLabel;
     dbDetails.type = 'MongoDB';
 
     console.log('Connected to MongoDB at: %s', mongoURL);
+
   });
+
+
+  // mongodb.connect(mongoURL, function(err, conn) {
+  //   if (err) {
+  //     callback(err);
+  //     return;
+  //   }
+
+  //   db = conn;
+  //   dbDetails.databaseName = db.databaseName;
+  //   dbDetails.url = mongoURLLabel;
+  //   dbDetails.type = 'MongoDB';
+
+  //   console.log('Connected to MongoDB at: %s', mongoURL);
+  // });
 };
+
+
+
+var routes = require('./routes/auth');
+app.use('/auth', routes)
+
+
+
 
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
