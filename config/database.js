3850ce197bd4df1e6ec536c3bab73052f132086c
testnodeemail123 <testnodeemail123@gmail.com>
2017-12-02 08:08:49 +0530

    updated database url

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     // 'url' : 'mongodb-globaldb1.193b.starter-ca-central-1.openshiftapps.com'
-    // 'url' : 'admin:i6SiypdEm7xvPlyk@mongodb-globaldb1.193b.starter-ca-central-1.openshiftapps.com:27017/admin'
-    'url' : 'mongodb://userM66:xYUyQ8FcTuUAmdpX@mongodb-globaldb1.193b.starter-ca-central-1.openshiftapps.com:27017/globaldb1'
+    'url' : 'mongodb://admin:i6SiypdEm7xvPlyk@mongodb-globaldb1.193b.starter-ca-central-1.openshiftapps.com:27017/admin'
+    // 'url' : 'mongodb://userM66:xYUyQ8FcTuUAmdpX@mongodb-globaldb1.193b.starter-ca-central-1.openshiftapps.com:27017/globaldb1'
 
 };
